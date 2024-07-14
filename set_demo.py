d2eac001f0a46b657038421b01be126cdce5f73e
Nasim Bahari <bahari.nsm@gmail.com>
2024-07-15 00:32:49 +0330

    Second Commit for 'set' Data Type

--- set_demo.py
+++ set_demo.py
@@ -1,5 +1,5 @@
 # unordered data type
-
+# mutable
 # you 'CANNOT' define empty set  using literal
 
 emptySet = set()
@@ -8,7 +8,7 @@ sampleSet = {1, 'A', 'g', 3.86, 'Dayche', 'A', 'A'}
 for _ in range(10):
     print(sampleSet)
 
-print('=' * 40)
+
 
 odd = set(range(1, 50, 2))
 power = {1, 4, 9, 16, 25, 36, 49}
